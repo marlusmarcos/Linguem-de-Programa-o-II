cf8efbe19bbc7e45e9238da6666eba23b9b792e7
marlusmarcos <38912986+marlusmarcos@users.noreply.github.com>
2019-08-14 15:22:29 -0300

    Update Main_contagem.java

--- Main_contagem.java
+++ Main_contagem.java
@@ -3,13 +3,21 @@ import java.util.Arrays;
 public class Main {
     
 	public static void main(String[] args) 
-	{
+	{	/*
+		frase na qual quero contar quantas vezes aparece uma determinada palavra
+		*/
 		String frase = "Eu tenho um barco vermelho e um carro vermelho";
 		String array[] = new String[10];
 		int cont = 0;
+		/*
+		cada posição do array é ocupada com palavras que são pegas através de um espaço " ".
+		ex: a cada espaço dado, uma palavra nova é add!
+		*/
 		array = frase.split(" ");	
 		for (String m_str : array) 
-		{
+		{	/*
+			função "CONTAINS, conta o número de ocorrenciar da palavra desejada"
+			*/
 			if (m_str.contains("vermelho")) 
 			{
 				cont += 1;
@@ -17,6 +25,7 @@ public class Main {
 		
 
 		}
+		//imprimindo o número de ocorrencias.
 		System.out.println (cont);
 
 	}
