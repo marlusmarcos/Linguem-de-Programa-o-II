a8faaca0e8ca0bd3d60552c1d9e2c1388f2d8646
marlusmarcos <38912986+marlusmarcos@users.noreply.github.com>
2019-08-20 17:07:47 -0300

    Create project.java

---
+++ project.java
@@ -0,0 +1,16 @@
+
+public class Professor {
+	protected String name; //the attribute refers to the teacher's name
+	protected String registration; // the attribute refers to the registration  
+	
+	public void setName (String n1) { // this method have the function to assign  the state name to the object  
+		this.name= n1;
+	}
+	/*
+	 * this method refer to the registration of teacher. that is represented by a string 
+	 * */
+	public void setRegistration (String r1) {
+		this.registration = r1; 
+	}
+
+}
